f0c6b8cd6c0fb82b74f35010faa42c3be134e395
Sárándi <sarandi.balint@gmail.com>
2022-09-22 13:49:01 +0200

    kinezet

---
+++ app/src/main/java/hu/petrik/gyakorlo0922/MainActivity.java
@@ -0,0 +1,16 @@
+package hu.petrik.gyakorlo0922;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Gyakorlo");
+    }
+}
